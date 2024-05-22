e30561d23c6aae5f53668fad708cf5f3ab5bcd30
mxway <mxway@163.com>
2024-05-22 15:22:22 +0800

    修改cmake编译工程及源码文件，支持多操作系统平台，多编译器编译

--- source/demo/360SafeDemo/main.cpp
+++ source/demo/360SafeDemo/main.cpp
@@ -2,14 +2,17 @@
 #include <UIFileHelper.h>
 #include "MainFrame.h"
 
-
-#ifdef _MSC_VER
+//对于windows系统下使用vs编译器，在debug版本下直接使用main函数运行带有控制台的界面程序。
+//对于release版本则直接使用WinMain运行时不带控制台信息。
+//在windows以及linux系统下使用gcc编译器，所有入口统一使用main
+#if defined(_MSC_VER) && defined(NDEBUG)
+#pragma comment(linker,"/subsystem:windows")
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR cmdLine, int show){
+                   LPSTR cmdLine, int show) {
     int argc = __argc;
     char** argv = __argv;
 #else
-int main(int argc, char **argv) {
+    int main(int argc, char** argv) {
 #endif
     UIResourceMgr::GetInstance().Init(argc, argv);
     UIString resourcePath = UIResourceMgr::GetInstance().GetCurrentPath() +
